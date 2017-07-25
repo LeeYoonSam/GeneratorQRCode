b6bc0ea66794adf919cbdbe5c2fe65ad47f7223f
Albert.lee <albert.l@cheilpengtai.com>
2017-07-25 18:01:07 +0900

    Generator QR Code

---
+++ app/src/main/java/qrcode/ys/com/generatorqrcode/GeneratorQRCode.java
@@ -0,0 +1,76 @@
+package qrcode.ys.com.generatorqrcode;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+
+public class GeneratorQRCode extends Activity implements View.OnClickListener {
+
+    private String LOG_TAG = "GenerateQRCode";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_generatorqr);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.button1:
+                EditText qrInput = (EditText) findViewById(R.id.qrInput);
+                String qrInputText = qrInput.getText().toString();
+                Log.v(LOG_TAG, qrInputText);
+
+                //Find screen size
+                WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+                Display display = manager.getDefaultDisplay();
+                Point point = new Point();
+                display.getSize(point);
+                int width = point.x;
+                int height = point.y;
+                int smallerDimension = width < height ? width : height;
+
+                // 화면을 3/4 비율로 사이즈 계산
+                smallerDimension = smallerDimension * 3/4;
+
+
+//                QRCodeEncoder(String data, Bundle bundle, String type, String format, int dimension)
+                //Encode with a QR Code image
+                QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(qrInputText,
+                        null,
+                        Contents.Type.TEXT,
+                        BarcodeFormat.QR_CODE.toString(),
+                        smallerDimension);
+                try {
+                    Bitmap bitmap = qrCodeEncoder.encodeAsBitmap();
+                    ImageView myImage = (ImageView) findViewById(R.id.imageView1);
+                    myImage.setImageBitmap(bitmap);
+
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+
+
+                break;
+
+            // More buttons go here (if any) ...
+
+        }
+    }
+}
